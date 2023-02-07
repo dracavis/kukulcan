ffc10e22e12ed67d319fdc8df0e606f10e248f4b
dracavis <95973586+dracavis@users.noreply.github.com>
2023-02-07 20:24:21 +0900

    update keymap.rb

--- kukulcan_xiao/keymap.rb
+++ kukulcan_xiao/keymap.rb
@@ -6,6 +6,7 @@ kbd = Keyboard.new
 kbd.via = true
 kbd.via_layer_count = 3
 kbd.split = true
+kbd.split_style = :right_side_flipped_split
 
 kbd.uart_pin = 1
 
@@ -39,7 +40,7 @@ kbd.add_layer :default, %i(
   KC_ESC  KC_1    KC_2    KC_3    KC_4    KC_5    KC_NO   KC_NO        KC_NO    KC_6           KC_7          KC_8           KC_9           KC_0     KC_BSPC KC_DEL
   KC_TAB  KC_Q    KC_W    KC_E    KC_R    KC_T    KC_NO   KC_NO        KC_NO    KC_Y           KC_U          KC_I           KC_O           KC_P     KC_MINS KC_INS
   KC_1    KC_2    KC_A    KC_S    KC_D    KC_F    KC_G    KC_NO        KC_H     KC_J           KC_K          KC_L           KC_SCLN        KC_NO    KC_ENT  KC_HOME
-  KC_LSFT KC_NO   KC_Z    KC_X    KC_C    KC_V    KC_B    KC_NO        KC_B     KC_N           KC_M          KC_DOT         KC_COMMA       KC_SLSH  KC_UP   KC_END
+  KC_LSFT KC_NO   KC_Z    KC_X    KC_C    KC_V    KC_B    KC_NO        KC_B     KC_N           KC_M          KC_COMM        KC_DOT         KC_SLSH  KC_UP   KC_END
   KC_LCTL KC_LGUI KC_LALT L1      KC_NO   KC_SPC  KC_NO   KC_NO        SPC_L2   KC_MHEN        L2            KC_RCTL        KC_NO          KC_LEFT  KC_DOWN KC_RGHT
 )
 kbd.add_layer :L1, %i(
